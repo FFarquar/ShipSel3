4f5fb03a02301190263dcbef05c58ab5fc4ef8db
FFarquar <110162452+FFarquar@users.noreply.github.com>
2025-01-01 14:05:40 +1100

    Changed folder structure to put shared library in same folder as WASM folder. This is to allow GIT to add the code to source control

---
+++ SharedLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace SharedLibrary
+{
+    //test class to see if other projects can read this
+    public class TestClass
+    {
+        public string ReturnTestMessage()
+        {
+            return "Returned message";
+        }
+    }
+}
